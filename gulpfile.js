458cf8b871133a258776fdb6a31e5eb50b5b9c1a
Maya Mircheva <may4eto@hotmail.com>
2020-12-03 11:05:54 +0100

    Optimized version of the landing page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,68 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+var gulp = require('gulp')
+var cleanCss = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
+var browserSync = require('browser-sync').create()
+var imagemin = require('gulp-imagemin')
+var postcss = require ('gulp-postcss')
+var concat = require('gulp-concat')
 
-exports.default = defaultTask
+gulp.task("css", function(){
+return gulp.src([
+    'src/css/reset.css',
+    'src/css/typography.css',
+    'src/css/style.css'
+])
+.pipe(sourcemaps.init())
+.pipe(
+    postcss([
+        require('autoprefixer'),
+        require('postcss-preset-env')({
+            stage: 1,
+            browsers: ['IE 11', 'last 2 versions']
+        })
+    ])
+)
+.pipe(concat('style.css'))
+.pipe(
+    cleanCss({
+        compatibility: 'ie8'
+    })
+)
+.pipe(sourcemaps.write())
+.pipe(gulp.dest("dist"))
+.pipe(browserSync.stream())
+})
+
+gulp.task("html", function (){
+    return gulp.src("src/*.html")
+    .pipe(gulp.dest("dist"))
+})
+
+gulp.task("fonts", function(){
+    return gulp.src("src/fonts/*")
+    .pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function() {
+    return gulp.src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"))
+})
+
+gulp.task("watch", function(){
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    })
+    gulp.watch("src/*.html", gulp.series('html')).on("change", browserSync.reload)
+    gulp.watch("src/css/style.css", gulp.series('css'))
+    gulp.watch("src/fonts/*", gulp.series('fonts'))
+    gulp.watch("src/img/*", gulp.series('images'))
+})
+
+//var defaultTask = function() {}
+
+gulp.task("default", gulp.series('html', 'css', 'fonts', 'images', 'watch'));
+
+//exports.default = defaultTask*/
